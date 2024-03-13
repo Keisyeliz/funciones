413bfb0adcb7e99f52425f4e95ad244def65b3d1
Keisyeliz <158522128+Keisyeliz@users.noreply.github.com>
2024-03-12 21:20:43 -0500

    Add files via upload

---
+++ velocidad1.py
@@ -0,0 +1,5 @@
+def fnt_calcularvelocidad(distancia,tiempo):
+    print (distancia/tiempo)
+    
+fnt_calcularvelocidad(24,4)
+# donde el primer número representa la distancia y el segundo el tiempo
